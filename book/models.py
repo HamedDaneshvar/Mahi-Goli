67924fdcf6c739e96156ff51be00d848a80945fb
Hamed Daneshvar <hmddaneshvar@gmail.com>
2022-01-15 10:37:19 +0330

    create book app & Person Model

---
+++ book/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Person(models.Model):
+    first_name = models.CharField(max_length=75, null=False, blank=False)
+    middle_name = models.CharField(max_length=75)
+    last_name = models.CharField(max_length=75, null=False, blank=False)
+    avatar = models.ImageField(upload_to='/images/person/')
+
+    def __str__(self):
+        return self.first_name + ' ' + self.middle_name + ' ' + self.last_name
