de4293bcd185e6b7fe6673603bc0d97d46a22698
Hamed Daneshvar <hmddaneshvar@gmail.com>
2022-01-16 19:52:35 +0330

    add book model and teller model

--- book/admin.py
+++ book/admin.py
@@ -4,4 +4,6 @@ from book.models import *
 # Register your models here.
 admin.site.register(Author)
 admin.site.register(Translator)
+admin.site.register(Teller)
 admin.site.register(Category)
+admin.site.register(Book)
--- book/models.py
+++ book/models.py
@@ -32,6 +32,13 @@ class Translator(AbstractPerson):
         verbose_name_plural = "مترجمان"
 
 
+class Teller(AbstractPerson):
+    
+    class Meta:
+        verbose_name = "گوینده"
+        verbose_name_plural = "گویندگان"
+
+
 class Category(models.Model):
     title = models.CharField(max_length=150, null=True, verbose_name="دسته‌بندی")
     parent = models.ForeignKey('self', default=True, null=True, blank=True, on_delete=models.SET_NULL,
@@ -39,4 +46,51 @@ class Category(models.Model):
 
     class Meta:
         verbose_name = "دسته‌بندی"
-        verbose_name_plural = "دسته‌بندی‌ها"    
+        verbose_name_plural = "دسته‌بندی‌ها"
+
+
+def file_format_validator(file_name) -> bool:
+    """validate format file of book"""
+    from django.core.exceptions import ValidationError
+    formats = ('.pdf', '.epub', '.rar', '.zip')
+    if not file_name[file_name.rfind('.'):].lower() in formats:
+        raise ValidationError("Unsupported file extension.")
+		
+		
+class Book(models.Model):
+    LANGUAGE_CHOICES = (
+        ('eng', 'انگلیسی'),
+        ('per', 'فارسی'),
+    )
+    RATE_CHOICES = (
+        (1, 'بد'),
+        (2, 'بد نبود'),
+        (3, 'معمولی'),
+        (4, 'خوب'),
+        (5, 'عالی'),
+    )
+    BOOK_TYPES = (
+        ('a', 'صوتی'),
+        ('p', 'فیزیکی'),
+        ('e', 'الکترونیکی'),
+    )
+    title = models.CharField(max_length=1500, verbose_name="نام کتاب")
+    author = models.ManyToManyField(Author, verbose_name="نویسنده")
+    translator = models.ManyToManyField(Translator, blank=True, verbose_name="مترجم")
+    teller = models.ManyToManyField(Teller, blank=True, verbose_name="گوینده")
+    language_book = models.CharField(max_length=3, choices=LANGUAGE_CHOICES, default="per")
+    book_url = models.URLField(max_length=1024, null=True, blank=True, default=None)
+    book_file = models.FileField(upload_to='./book_files/', blank=True, validators=[file_format_validator])
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL, verbose_name="دسته‌بندی")
+    price = models.DecimalField(max_digits=12, decimal_places=2, null=True, blank=True, verbose_name="قیمت")
+    user_description = models.TextField(null=True, blank=True, verbose_name="توضیحات")
+    rate = models.SmallIntegerField(choices=RATE_CHOICES, null=True, blank=True, verbose_name="امتیاز")
+    book_type = models.CharField(max_length=1, choices=BOOK_TYPES, default="e", verbose_name="نوع کتاب")
+
+
+    class Meta:
+        verbose_name = "کتاب"
+        verbose_name_plural = "کتب"
+
+    def __str__(self) -> str:
+        return self.title
