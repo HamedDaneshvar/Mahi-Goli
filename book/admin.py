f9c7df93056113d88e1b9863118f9da7230c012a
Hamed Daneshvar <hmddaneshvar@gmail.com>
2022-01-16 18:53:55 +0330

    modify person model to use it for author and translator model

--- book/admin.py
+++ book/admin.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from book.models import *
 
 # Register your models here.
-admin.site.register(Person)
+admin.site.register(Author)
+admin.site.register(Translator)
 admin.site.register(Category)
--- book/models.py
+++ book/models.py
@@ -1,19 +1,35 @@
-from tabnanny import verbose
 from django.db import models
 
 # Create your models here.
-class Person(models.Model):
+# abstract person model for inheritance author and translator model
+class AbstractPerson(models.Model):
     first_name = models.CharField(max_length=75, null=False, blank=False, verbose_name="نام")
-    middle_name = models.CharField(max_length=75, verbose_name="نام میانی")
+    middle_name = models.CharField(max_length=75, blank=True, default='', verbose_name="نام میانی")
     last_name = models.CharField(max_length=75, null=False, blank=False, verbose_name="نام خانوادگی")
-    avatar = models.ImageField(upload_to='./images/person/', verbose_name="عکس پروفایل")
+    avatar = models.ImageField(upload_to='./images/person/', blank=True, verbose_name="عکس پروفایل")
+    
 
     class Meta:
         verbose_name = "کاربر"
         verbose_name_plural = "کاربران"
+        abstract = True
 
     def __str__(self):
         return self.first_name + ' ' + self.middle_name + ' ' + self.last_name
+
+
+class Author(AbstractPerson):
+    
+    class Meta:
+        verbose_name = "نویسنده"
+        verbose_name_plural = "نویسندگان"
+
+
+class Translator(AbstractPerson):
+    
+    class Meta:
+        verbose_name = "مترجم"
+        verbose_name_plural = "مترجمان"
 
 
 class Category(models.Model):
