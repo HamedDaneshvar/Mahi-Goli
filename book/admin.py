3483a921fd54365519ff9553ced55bd43d0094d0
Hamed Daneshvar <hmddaneshvar@gmail.com>
2022-01-25 09:52:49 +0330

    add custome translator and custome teller to admin panel

--- book/admin.py
+++ book/admin.py
@@ -7,9 +7,19 @@ class AuthorAdmin(admin.ModelAdmin):
     search_fields = ['first_name', 'middle_name', 'last_name']
     
 
+class TranslatorAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'middle_name', 'last_name']
+    search_fields = ['first_name', 'middle_name', 'last_name']
+
+
+class TellerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'middle_name', 'last_name']
+    search_fields = ['first_name', 'middle_name', 'last_name']
+
+
 admin.site.register(Author, AuthorAdmin)
-admin.site.register(Translator)
-admin.site.register(Teller)
+admin.site.register(Translator, TranslatorAdmin)
+admin.site.register(Teller, TellerAdmin)
 admin.site.register(Category)
 admin.site.register(Publisher)
 admin.site.register(Book)
