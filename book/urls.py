59fed554d14a436635c04295a36426511d1ed4d4
Hamed Daneshvar <hmddaneshvar@gmail.com>
2022-02-01 13:54:32 +0330

    show physical book in tempalte

--- book/urls.py
+++ book/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import *
+from .views import index, PhysicalBook
 
 app_name = 'book'
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('physicalbook/', PhysicalBook.as_view(), name='physicalbook_list'),
 ]
--- book/views.py
+++ book/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import PhysicalBook
 
 # Create your views here.
 def index(request):
     return render(request, 'book/index.html')
+
+
+class PhysicalBook(ListView):
+    model = PhysicalBook
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
