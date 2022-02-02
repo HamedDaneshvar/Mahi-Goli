b49bde5d5832fe2384b76147b5eecf75c3aed853
Hamed Daneshvar <hmddaneshvar@gmail.com>
2022-02-02 06:38:22 +0330

    dynamic url for sidebar

--- book/urls.py
+++ book/urls.py
@@ -14,12 +14,12 @@ from .views import (
 app_name = 'book'
 urlpatterns = [
     path('', index, name='index'),
-    path('physicalbook/', PhysicalBook.as_view(), name='physicalbook_list'),
-    path('electronicbook/', ElectronicBook.as_view(), name='electronicbook_list'),
-    path('audiobook/', AudioBook.as_view(), name='audiobook_list'),
-    path('author/', Author.as_view(), name='author_list'),
-    path('translator/', Translator.as_view(), name='translator_list'),
-    path('teller/', Teller.as_view(), name='teller_list'),
-    path('publisher/', Publisher.as_view(), name='publisher_list'),
-    path('category/', Category.as_view(), name='category_list'),
+    path('physicalbook/', PhysicalBook.as_view(), name='physicalbook'),
+    path('electronicbook/', ElectronicBook.as_view(), name='electronicbook'),
+    path('audiobook/', AudioBook.as_view(), name='audiobook'),
+    path('author/', Author.as_view(), name='author'),
+    path('translator/', Translator.as_view(), name='translator'),
+    path('teller/', Teller.as_view(), name='teller'),
+    path('publisher/', Publisher.as_view(), name='publisher'),
+    path('category/', Category.as_view(), name='category'),
 ]
