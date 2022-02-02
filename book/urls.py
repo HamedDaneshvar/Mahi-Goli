da3592310370fd15f110b50982844deda086e382
Hamed Daneshvar <hmddaneshvar@gmail.com>
2022-02-02 05:09:11 +0330

    add three person model(author, translator, teller) to the panel

--- book/urls.py
+++ book/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
-from .views import index, PhysicalBook, ElectronicBook, AudioBook
+from .views import (
+    index,
+    PhysicalBook,
+    ElectronicBook,
+    AudioBook,
+    Author,
+    Translator,
+    Teller
+)
 
 app_name = 'book'
 urlpatterns = [
@@ -7,4 +15,7 @@ urlpatterns = [
     path('physicalbook/', PhysicalBook.as_view(), name='physicalbook_list'),
     path('electronicbook/', ElectronicBook.as_view(), name='electronicbook_list'),
     path('audiobook/', AudioBook.as_view(), name='audiobook_list'),
+    path('author/', Author.as_view(), name='author_list'),
+    path('translator/', Translator.as_view(), name='translator_list'),
+    path('teller/', Teller.as_view(), name='teller_list'),
 ]
--- book/views.py
+++ book/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import ListView
-from .models import PhysicalBook, ElectronicBook, AudioBook
+from .models import (
+    PhysicalBook,
+    ElectronicBook,
+    AudioBook,
+    Author,
+    Translator,
+    Teller,
+)
 
 # Create your views here.
 def index(request):
@@ -28,4 +35,34 @@ class AudioBook(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        return context
+
+
+class Author(ListView):
+    model = Author
+    template_name = 'book/person_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['model_title'] = "نویسندگان"
+        return context
+
+
+class Translator(ListView):
+    model = Translator
+    template_name = 'book/person_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['model_title'] = "مترجمین"
+        return context
+
+
+class Teller(ListView):
+    model = Teller
+    template_name = 'book/person_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['model_title'] = "گویندگان"
         return context
