301a1a31eca2eba02edc201e35c58a2fd1d00959
Hamed Daneshvar <hmddaneshvar@gmail.com>
2022-02-02 04:18:21 +0330

    add electronic book model to the panel

--- book/urls.py
+++ book/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index, PhysicalBook
+from .views import index, PhysicalBook, ElectronicBook
 
 app_name = 'book'
 urlpatterns = [
     path('', index, name='index'),
     path('physicalbook/', PhysicalBook.as_view(), name='physicalbook_list'),
+    path('electronicbook/', ElectronicBook.as_view(), name='electronicbook_list'),
 ]
--- book/views.py
+++ book/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView
-from .models import PhysicalBook
+from .models import PhysicalBook, ElectronicBook
 
 # Create your views here.
 def index(request):
@@ -10,6 +10,14 @@ def index(request):
 class PhysicalBook(ListView):
     model = PhysicalBook
     
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+
+class ElectronicBook(ListView):
+    model = ElectronicBook
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         return context
