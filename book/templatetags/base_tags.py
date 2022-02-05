f809f0ef176160b051a9ff6e913c5248c5043cf0
Hamed Daneshvar <hmddaneshvar@gmail.com>
2022-02-05 10:46:00 +0330

    fix bug of side bar for active and show opening and closing list item for two level top down

--- book/templatetags/base_tags.py
+++ book/templatetags/base_tags.py
@@ -16,14 +16,31 @@ def sidebar_item(request, link_name, content, icon_classes):
 
 @register.simple_tag
 def active_book_item(request, *args):
-    book_url_names = ('physicalbook', 'electronicbook', 'audiobook', 'allbook')
-    if resolve(request.path_info).url_name in book_url_names:
-        if args[0] == 'menu-open':
+    url_name = resolve(request.path_info).url_name
+    book_url_names = {'physicalbook': ('physicalbook'), 'electronicbook': ('',), 'audiobook': ('',), 'allbook': ('',)}
+
+    parent_url = None
+    if url_name not in book_url_names.keys():
+        for k, v in book_url_names.items():
+            if url_name in v:
+                parent_url = k
+
+    if url_name in book_url_names.keys() or parent_url in book_url_names.keys():
+        if args[0] == 'menu-open' and args[1] == 'parent':
             return 'menu-open'
-        elif args[0] == 'active':
-            return 'active'
-        elif args[0] == 'display':
+        elif args[0] == 'menu-open' and args[1] != 'parent':
+            if url_name in book_url_names.get(url_name, []) or parent_url in book_url_names.keys():
+                return 'menu-open'
+        elif args[0] == 'display' and args[1] == 'parent':
             return 'block'
+        elif args[0] == 'display' and args[1] != 'parent':
+            if url_name in book_url_names.get(url_name, []) or parent_url in book_url_names.keys():
+                return 'block'
+        elif args[0] == 'active' and args[1] == 'parent':
+            return 'active'
+        elif args[0] == 'active' and args[1] != 'parent':
+            if url_name in book_url_names.get(url_name, []) or parent_url in book_url_names.keys():
+                return 'active'
     else:
         if args[0] == 'display':
             return 'none'
