f147bf97e7efffbc4cf1cc4d5fec8ae161d8b44a
Hamed Daneshvar <hmddaneshvar@gmail.com>
2022-02-06 02:20:21 +0330

    fix some bug from sidebar and add list item for every type of book

--- book/templatetags/base_tags.py
+++ book/templatetags/base_tags.py
@@ -17,29 +17,32 @@ def sidebar_item(request, link_name, content, icon_classes):
 @register.simple_tag
 def active_book_item(request, *args):
     url_name = resolve(request.path_info).url_name
-    book_url_names = {'physicalbook': ('physicalbook'), 'electronicbook': ('',), 'audiobook': ('',), 'allbook': ('',)}
+    book_url_names = {'physicalbook': ('physicalbook', 'physicalbook_create'), 
+                    'electronicbook': ('electronicbook',), 
+                    'audiobook': ('audiobook',), 
+                    'allbook': ('allbook',)}
 
     parent_url = None
     if url_name not in book_url_names.keys():
         for k, v in book_url_names.items():
             if url_name in v:
                 parent_url = k
-
+    print(url_name, parent_url)
     if url_name in book_url_names.keys() or parent_url in book_url_names.keys():
         if args[0] == 'menu-open' and args[1] == 'parent':
             return 'menu-open'
         elif args[0] == 'menu-open' and args[1] != 'parent':
-            if url_name in book_url_names.get(url_name, []) or parent_url in book_url_names.keys():
+            if (url_name in book_url_names.get(url_name, []) and args[1] == url_name) or (parent_url in book_url_names.keys() and args[1] == parent_url):
                 return 'menu-open'
         elif args[0] == 'display' and args[1] == 'parent':
             return 'block'
         elif args[0] == 'display' and args[1] != 'parent':
-            if url_name in book_url_names.get(url_name, []) or parent_url in book_url_names.keys():
+             if (url_name in book_url_names.get(url_name, []) and args[1] == url_name) or (parent_url in book_url_names.keys() and args[1] == parent_url):
                 return 'block'
         elif args[0] == 'active' and args[1] == 'parent':
             return 'active'
         elif args[0] == 'active' and args[1] != 'parent':
-            if url_name in book_url_names.get(url_name, []) or parent_url in book_url_names.keys():
+            if (url_name in book_url_names.get(url_name, []) and args[1] == url_name) or (parent_url in book_url_names.keys() and args[1] == parent_url):
                 return 'active'
     else:
         if args[0] == 'display':
